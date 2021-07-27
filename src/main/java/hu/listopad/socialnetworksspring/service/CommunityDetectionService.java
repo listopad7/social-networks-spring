94c7b77bb542484b5b12e8ba32d764378a09c466
Noemi Czuczy <noemi.czuczy@gmail.com>
2021-07-27 15:39:34 +0200

    First commit, far from working yet.

---
+++ src/main/java/hu/listopad/socialnetworksspring/service/CommunityDetectionService.java
@@ -0,0 +1,8 @@
+package hu.listopad.socialnetworksspring.service;
+
+/**
+ * Created by Noemi Czuczy on 2021. 07. 27.
+ */
+public interface CommunityDetectionService {
+
+}
---
+++ src/main/java/hu/listopad/socialnetworksspring/service/LouvainService.java
@@ -0,0 +1,19 @@
+package hu.listopad.socialnetworksspring.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Noemi Czuczy on 2021. 07. 27.
+ */
+public class LouvainService {
+
+	WeightedGraph g;
+
+	Lo
+
+	public List<LouvainResult> getCommunityDetectionResults() {
+		List<LouvainResult> result = new ArrayList<>();
+
+	}
+}
